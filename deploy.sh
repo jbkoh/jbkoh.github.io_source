22a2bfb7a0c540f4e18e9b56d702dab00baed733
Jason <bk7749@gmail.com>
2017-01-02 18:55:09 -0800

    src update

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ git commit -m "src update"
 git push -f origin master
 hugo
 cd public
-git add --all
+git init
+git remote add origin https://github.com/jbkoh/jbkoh.github.io.git
+git add * -A
 git commit -m "update"
 git push -f origin master
