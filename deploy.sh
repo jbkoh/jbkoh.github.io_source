80cdf7fecf110bf47b359dd9c0d797929e1f52b9
Jason Koh <bk7749@gmail.com>
2019-06-04 13:00:31 -0700

    src update

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ hugo
 cd public
 rm -rf .git
 git init
-git remote add origin https://github.com/jbkoh/jbkoh.github.io.git
+git remote add origin git@github.com:jbkoh/jbkoh.github.io.git
 git add * -A
 git commit -m "update"
 git push -f origin master
